b7dfeafc1fd7adcfbfaa3df394c5508ad8ed889e
Marcell Tanure <59565705+marcellpaganini@users.noreply.github.com>
2022-08-25 11:27:58 -0300

    Complete demo

--- mission-project/src/app/app.module.ts
+++ mission-project/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MissionControlComponent } from './missioncontrol/missioncontrol.component';
+import { AstronautComponent } from './astronaut/astronaut.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MissionControlComponent,
+    AstronautComponent
   ],
   imports: [
     BrowserModule,
